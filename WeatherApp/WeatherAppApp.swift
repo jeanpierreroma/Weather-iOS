51e139ef2dc316098b48b06e1cd3d8c858bcb977
Roman <ppgora352@gmail.com>
2025-07-10 06:11:46 -0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Oleksandr Melnechenko on 10.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
