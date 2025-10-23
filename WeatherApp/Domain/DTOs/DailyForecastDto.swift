54d55ef1ae978c67f88bfb43d12232625b717836
Roman <ppgora352@gmail.com>
2025-10-23 14:09:11 +0300

    feat(domain): add DailyForecastDto for weather data transfer objects

---
+++ WeatherApp/Domain/DTOs/DailyForecastDto.swift
@@ -0,0 +1,93 @@
+//
+//  DailyForecastDto.swift
+//  WeatherApp
+//
+//  Created by Oleksandr Melnechenko on 23.10.2025.
+//
+
+import Foundation
+
+struct DailyForecastDto: Sendable, Decodable {
+    let airQualityDetailsDto: AirQualityDetailsDto
+    let feelsLikeDetailsDto: FeelsLikeDetailsDto
+    let humidityDetailsDto: HumidityDetailsDto
+    let precipitationDetailsDto: PrecipitationDetailsDto
+    let pressureDetailsDto: PressureDetailsDto
+    let sunriseSunsetDetailsDto: SunriseSunsetDetailsDto
+    let uvDetailsDto: UvDetailsDto
+    let visibilityDetailsDto: VisibilityDetailsDto
+    let windDetailsDto: WindDetailsDto
+    
+    private enum CodingKeys: String, CodingKey {
+        case airQualityDetails
+        case feelsLikeDetails
+        case humidityDetails
+        case precipitationDetails
+        case pressureDetails
+        case sunDetails
+        case uvDetails
+        case visibilityDetails
+        case windDetails
+    }
+
+    init(from decoder: Decoder) throws {
+        let c = try decoder.container(keyedBy: CodingKeys.self)
+        airQualityDetailsDto     = try c.decode(AirQualityDetailsDto.self,     forKey: .airQualityDetails)
+        feelsLikeDetailsDto      = try c.decode(FeelsLikeDetailsDto.self,      forKey: .feelsLikeDetails)
+        humidityDetailsDto       = try c.decode(HumidityDetailsDto.self,       forKey: .humidityDetails)
+        precipitationDetailsDto  = try c.decode(PrecipitationDetailsDto.self,  forKey: .precipitationDetails)
+        pressureDetailsDto       = try c.decode(PressureDetailsDto.self,       forKey: .pressureDetails)
+        sunriseSunsetDetailsDto  = try c.decode(SunriseSunsetDetailsDto.self,  forKey: .sunDetails)
+        uvDetailsDto             = try c.decode(UvDetailsDto.self,             forKey: .uvDetails)
+        visibilityDetailsDto     = try c.decode(VisibilityDetailsDto.self,     forKey: .visibilityDetails)
+        windDetailsDto           = try c.decode(WindDetailsDto.self,           forKey: .windDetails)
+    }
+}
+
+struct AirQualityDetailsDto: Sendable, Decodable {
+    let airQualityIndex: Double
+    let airQualityDetailsCategoryName: String
+    let summary: String
+}
+
+struct FeelsLikeDetailsDto: Sendable, Decodable {
+    let feelsLikeTemperature: Double
+    let summary: String
+}
+
+struct HumidityDetailsDto: Sendable, Decodable {
+    let humidityPercent: Int
+    let summary: String
+}
+
+struct PrecipitationDetailsDto: Sendable, Decodable {
+    let precipitationDuringLast24Hours: Double
+    let summary: String
+}
+
+struct PressureDetailsDto: Sendable, Decodable {
+    let pressureHpa: Int
+    let summary: String
+}
+
+struct SunriseSunsetDetailsDto: Sendable, Decodable {
+    let sunriseText: String
+    let sunsetText: String
+}
+
+struct UvDetailsDto: Sendable, Decodable {
+    let uvIndexValueMax: Double
+    let uvIndexRiskCategoryName: String
+    let summary: String
+}
+
+struct VisibilityDetailsDto: Sendable, Decodable {
+    let visibilityKm: Double
+    let summary: String
+}
+ 
+struct WindDetailsDto: Sendable, Decodable {
+    let windSpeedMps: Double
+    let gustSpeedMps: Double
+    let directionDegrees: Int
+}
